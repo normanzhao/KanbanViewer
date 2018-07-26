e2020cf4653807f0d77efedb590efa8371c9d7d8
Norman Zhao <normanlz.zhao@gmail.com>
2018-07-25 20:14:51 -0400

    Hit API to get data for all projects and associated items. The icons are a bit skewed, but everything else works fine.

--- src/app/kanbanviewer/kanbanviewer.component.ts
+++ src/app/kanbanviewer/kanbanviewer.component.ts
@@ -13,7 +13,14 @@ export class KanbanviewerComponent implements OnInit {
   constructor(private kbviewerService: KanbanviewerService) { }
 
   public projects;
-  selectedProject: string = '1';
+  openItems = [];
+  ongoingItems = [];
+  closedItems = [];
+  releasedItems = [];
+  selectedProject: string = '0';
+
+  priority = ["fas fa-angle-double-down", "fas fa-angle-down", "far fa-circle", "fas fa-angle-up", "fas fa-angle-double-up"];
+  type = { Story: "fas fa-book", Feature: "fas fa-exclamation", Request: "fas fa-question", Bug: "fas fas fa-bug", Epic: "fas fa-bolt" };
 
   ngOnInit() {
     this.getData();
@@ -21,15 +28,42 @@ export class KanbanviewerComponent implements OnInit {
 
   getData() {
     this.kbviewerService.getData().subscribe(
-      data => { this.projects = data },
+      data => { this.projects = data; this.loadItems(this.selectedProject); },
       err => console.error(err),
       () => console.log()
     );
   }
 
+  loadItems(projectID) {
+    this.openItems = [];
+    this.ongoingItems = [];
+    this.closedItems = [];
+    this.releasedItems = [];
+
+    var projectItems = this.projects[projectID]["items"];
+    for (var itemID in projectItems) {
+      switch (projectItems[itemID].status) {
+        case "open":
+          this.openItems.push(projectItems[itemID]);
+          break;
+        case "ongoing":
+          this.ongoingItems.push(projectItems[itemID]);
+          break;
+        case "closed":
+          this.closedItems.push(projectItems[itemID]);
+          break;
+        case "released":
+          this.releasedItems.push(projectItems[itemID]);
+          break;
+        default:
+          break
+      }
+    }
+  }
+
   getSelectedProject(event: any) {
     this.selectedProject = event.target.value;
-    console.log(this.projects[this.selectedProject]);
+    this.loadItems(this.selectedProject);
   }
 
 }
